c0b4dda9de664de7f008f394a20117e276db055d
Bodo Bogdan-Mihai <80922283+waterAddicted@users.noreply.github.com>
2021-08-29 05:17:38 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+
+a = int(input())
+b = int(input())
+a = a + b
+b = a - b
+a = a - b
+print (a,b)
+
+c = int(input())
+d = int(input())
+c = c * d
+d = c // d
+c = c // d
+print (c,d)
+
+e = int(input())
+f = int(input())
+e = e ^ f
+f = e ^ f
+e = e ^ f
+print(e,f)
